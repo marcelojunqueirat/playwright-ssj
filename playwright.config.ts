ff4c75abcb455461a342a4f03208d68bef5f5fb3
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2022-07-20 15:33:13 -0300

    page object testing

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  reporter: [['html', {outputFolder: 'report'}]],
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'on',
+    video: 'on'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+};
+export default config;
---
+++ tests/login.spec.ts
@@ -0,0 +1,32 @@
+import { test } from '@playwright/test'
+import {LoginPage} from '../pages/login-page'
+
+let loginPage : LoginPage
+
+test.beforeEach(async ({page})=> {
+    loginPage = new LoginPage(page)
+})
+
+test('Deve logar com sucesso', async ({ page }) => {
+    await loginPage.go()
+    await loginPage.signIn('qa', 'cademy')
+    await loginPage.loggedIn()
+})
+
+test('Senha incorreta', async ({ page }) => {
+    await loginPage.go()
+    await loginPage.signIn('qa', 'abc123')
+    await loginPage.toastMessage('Oops! Credenciais inválidas :(')
+})
+
+test('Nome obrigatório', async ({page})=>{
+    await loginPage.go()
+    await loginPage.signIn('', 'abc123')
+    await loginPage.toastMessage('Informe o seu nome de usuário!')
+})
+
+test('Senha obrigatória', async ({ page }) => {
+    await loginPage.go()
+    await loginPage.signIn('qa', '')
+    await loginPage.toastMessage('Informe a sua senha secreta!')
+})
